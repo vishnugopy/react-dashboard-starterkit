0c8a6ef4c5a866154f61166a549740c8c0e4c617
vishnugopy <vishnugoby0010@gmail.com>
2021-06-28 14:48:26 +0200

    updates

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,11 +1,16 @@
 import React, { useEffect, useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
 import {
   Container,
   List,
   ListItemIcon,
+  ListItemSecondaryAction,
+  IconButton,
   ListItem,
   ListItemText,
 } from "@material-ui/core";
+import DeleteIcon from "@material-ui/icons/Delete";
+import EditIcon from "@material-ui/icons/Edit";
 import { apprenantService, promoService } from "../../services";
 
 const Dashboard = () => {
@@ -13,6 +18,27 @@ const Dashboard = () => {
   const [promos, setPromos] = useState([]);
   const [students, setStudents] = useState([]);
 
+  const useStyles = makeStyles((theme) => ({
+    maincontainer: {
+      width: "auto",
+      display: "flex",
+      
+    },
+    sub: {
+        border: '1px solid black',  
+        padding: '0',
+        margin:'2'
+    },
+    subheading :{
+        background : 'blue',
+        color : 'white',
+        padding : '1rem',
+        margin:'0'
+        
+    }
+  }));
+
+  const classes = useStyles();
   const getPromos = async () => {
     const Promos = await promoService.getAll();
     setPromos(Promos.data);
@@ -34,26 +60,46 @@ const Dashboard = () => {
     <Container>
       {error && <p>{error}</p>}
       <h1>Dashboard</h1>
-      <Container className="promocontainer">
-        <h2>Promos</h2>
-        <List>
-          {promos.map((promo, index) => (
-            <ListItem key={index} > 
-              <ListItemText primary={promo.name}></ListItemText>
-            </ListItem>
-          ))}
-        </List>
-      </Container>
+      <Container className={classes.maincontainer}>
+        <Container
+          maxWidth="40%"
+          className={classes.sub}
+        >
+          <h2 className={classes.subheading}>Promos</h2>
+          <List>
+            {promos.map((promo, index) => (
+              <ListItem key={index}>
+                <ListItemText primary={promo.name}></ListItemText>
+              </ListItem>
+            ))}
+          </List>
+        </Container>
 
-      <Container className="studentcontainer">
-      <h2>Apprenants</h2>
-        <List>
-          {students.map((student, index) => (
-            <ListItem key={index} > 
-              <ListItemText primary={student.firstname} secondary={student.lastname}></ListItemText>
-            </ListItem>
-          ))}
-        </List>
+        <Container
+          className="studentcontainer"
+          maxWidth="40%"
+          className={classes.sub}
+        >
+          <h2 className={classes.subheading}>Apprenants</h2>
+          <List>
+            {students.map((student, index) => (
+              <ListItem key={index}>
+                <ListItemText
+                  primary={student.firstname}
+                  secondary={student.lastname}
+                ></ListItemText>
+                <ListItemSecondaryAction>
+                  <IconButton edge="end" aria-label="delete">
+                    <EditIcon />
+                  </IconButton>
+                  <IconButton edge="end" aria-label="delete">
+                    <DeleteIcon />
+                  </IconButton>
+                </ListItemSecondaryAction>
+              </ListItem>
+            ))}
+          </List>
+        </Container>
       </Container>
     </Container>
   );
