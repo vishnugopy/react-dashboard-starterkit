7d7e5425d81247984b0b4aa47a9cc65d654f5244
vishnugopy <vishnugoby0010@gmail.com>
2021-06-28 11:02:45 +0200

    base

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,6 +1,7 @@
-import React, {useState} from 'react';
+import React, {useState , Redirect}  from 'react';
 import { Button, TextField } from '@material-ui/core';
 import useStyles from '../theme/forms.css';
+import { login } from '../services';
 
 const LoginForm = (props) => {
 
@@ -9,8 +10,13 @@ const LoginForm = (props) => {
     const [error, setError] = useState(null);
 
     const handleClick = async (e) => {
-        console.log(username, password);
-
+        const loginData = await login(username, password);
+        console.log(loginData);
+        if(loginData.data.success){
+            const {token} = loginData.data;
+            localStorage.setItem("token" , token);
+            props.history.push('/dashboard');
+        }
     }
 
     const classes = useStyles();
